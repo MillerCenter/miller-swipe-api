ba64605de0d1717459cbe7658eb48d0b77b0d6f8
Chris Hoyle <chrishoyle@Chriss-MacBook-Pro-2.local>
2018-02-14 11:55:43 -0500

    add config

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.url = "https://masonweb.wm.edu/swipe/?DisplayAll=true";
+config.username = "cthoyle";
+config.password = "WilliamMaryxc4!";
+config.domain = "CAMPUS";
+
+module.exports = config;
